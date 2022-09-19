07b9fa0a5f6d91340703e31db52c0f4df0b5e99d
RedNoyz <111233665+RedNoyz@users.noreply.github.com>
2022-09-19 19:45:25 +0300

    Changed Add button to "Save Credentials"
    Changed theme to Dark
    
    Added "copied to clipboard" text

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import os
 import random
+import sv_ttk
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -16,8 +17,6 @@ def generate_password():
     nr_numbers = random.randint(1, 3)
     nr_symbols = random.randint(1, 3)
 
-    # Eazy Level - Order not randomised:
-    # e.g. 4 letter, 2 symbol, 2 number = JduE&!91
     password = ""
 
     for letter in range(1, nr_letters + 1):
@@ -29,14 +28,12 @@ def generate_password():
     for symb in range(0, nr_symbols + 1):
         password += random.choice(symbols)
 
-    # Hard Level - Order of characters randomised:
-    # e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-
     pwd = list(password)
     random.shuffle(pwd)
     password_entry.delete(0, "end")
     password_entry.insert(0, "".join(pwd))
     password_entry.clipboard_append(password_entry.get())
+    save_password_label.config(text="Password copied to clipboard!")
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -85,11 +82,16 @@ password_label.grid(column=0, row=3)
 password_entry = Entry(width=31)
 password_entry.grid(column=1, row=3)
 
-add_button = Button(text="Add", command=save_credentials)
-add_button.grid(column=1, row=4)
+add_button = Button(text="Save Credentials", command=save_credentials)
+add_button.grid(column=1, row=5)
+add_button.config(pady=10, padx=10)
+
+save_password_label = Label(text="", font=("Arial", 8))
+save_password_label.grid(column=1, row=4)
 
 generate_password = Button(text="Generate Password", command=generate_password)
 generate_password.grid(column=2, row=3)
 
+sv_ttk.set_theme("dark")
 
 mainloop()
